19ea0fcc44c1a15bf4ca124f8ae110d6ce1c9f03
Tony Navarro & Vaughn Dice <student@epicodus.com>
2015-10-29 10:49:32 -0700

    Fix sorting and add asc/desc arrow icons.

--- app/controllers/calorie_counts_controller.rb
+++ app/controllers/calorie_counts_controller.rb
@@ -1,17 +1,21 @@
 class CalorieCountsController < ApplicationController
   def index
     @calories = CalorieCount.all
+    @exercises = Exercise.all
+    @foods = Food.all
 
     if params[:sort] && params[:sort].include?('calorie_count')
       @exercises = Exercise.includes(:calorie_count)
       @foods = Food.includes(:calorie_count)
-    else
-      @exercises = Exercise.all
-      @foods = Food.all
     end
 
-    # TODO: paginate in food/exercise or application controller
-    @exercises = @exercises.order(sort_column + ' ' + sort_direction).paginate(:page => params[:exercises_page], :per_page => 5)
-    @foods = @foods.order(sort_column + ' ' + sort_direction).paginate(:page => params[:foods_page], :per_page => 5)
+    if params[:class_name] && params[:class_name] == "exercise"
+      @exercises = @exercises.order(sort_column + ' ' + sort_direction)
+    else
+      @foods = @foods.order(sort_column + ' ' + sort_direction)
+    end
+
+    @exercises = @exercises.paginate(:page => params[:exercises_page], :per_page => 5)
+    @foods = @foods.paginate(:page => params[:foods_page], :per_page => 5)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-  def sortable(column, title = nil)
+  def sortable(column, class_name, title = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
-    link_to title, root_path(:sort => column, :direction => direction), {:class => css_class}
+    link_to title, root_path( :sort => column, :direction => direction, :class_name => class_name ), {:class => css_class}
   end
 end
